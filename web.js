4b25cdd3f5562365b39ea1dd8775e951fc09f5c3
Ian McKague <ianmckague@gmail.com>
2013-07-14 04:33:39 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -2,9 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  var page = fs.readFileSync('index.html');
+  response.send(page.toString());
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
